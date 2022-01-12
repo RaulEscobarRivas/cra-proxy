488aba66aa02e8ce38abb3a960541c80f9c696da
RaulEscobarRivas <raul.escobar@tourlane.com>
2022-01-12 12:40:25 +0100

    proxy setup

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm just a reverse proxy
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/reverse-proxy-basepath"
         >
-          Learn React
+          Go to NextJs App
         </a>
       </header>
     </div>
